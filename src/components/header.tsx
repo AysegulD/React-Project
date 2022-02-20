bde737775dc2a7b462afc2117d4264ce7fc69e06
Aysegul Demir <aydemgul@hotmail.com>
2022-02-20 19:15:17 +0000

    20/12

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -23,29 +23,46 @@ function getUser(){
       setUser(response.data)
       })
 }
+
 return (
       <div className="App">
         <div className='header'>
           <div className="row">
-            <div className="col">{user?.id}</div>
-            <div className="col">{user?.name}</div>
-            <div className="col">{user?.username}</div>
-            <div className="col">{user?.email}</div>
-            <div className="col">{user?.address.street}</div>
-            <div className="col">{user?.address.suite}</div>
+            <div className="col">ID
+            <div className="col">{user?.id}</div></div>
+            <div className="col">Name
+            <div className="col">{user?.name}</div></div>
+            <div className="col">UserName
+            <div className="col">{user?.username}</div></div>
+            <div className="col">Email
+            <div className="col">{user?.email}</div></div>
+            <div className="col">Street
+            <div className="col">{user?.address.street}</div></div>
+            <div className="col">Suite
+            <div className="col">{user?.address.suite}</div></div>
+           
           </div>
           <div className="row">
-            <div className="col">{user?.address.city}</div>
-            <div className="col">{user?.address.zipCode}</div>
-            <div className="col">{user?.address.geo.lat}</div>
-            <div className="col">{user?.address.geo.lng}</div>
-            <div className="col">{user?.phone}</div>
-            <div className="col">{user?.website}</div>
+            <div className="col">City
+            <div className="col">{user?.address.city}</div></div>
+            <div className="col">ZipCode
+            <div className="col">{user?.address.zipCode}</div></div>
+            <div className="col">Geo Lat
+            <div className="col">{user?.address.geo.lat}</div></div>
+            <div className="col">Geo Lng
+            <div className="col">{user?.address.geo.lng}</div></div>
+            <div className="col">Phone
+            <div className="col">{user?.phone}</div></div>
+            <div className="col">Website
+            <div className="col">{user?.website}</div></div>
           </div>
           <div className="row">
-            <div className="col">{user?.company.name}</div>
-            <div className="col">{user?.company.catchPhrase}</div>
-            <div className="col">{user?.company.bs}</div>
+          <div className="col">Company Name
+            <div className="col">{user?.company.name}</div></div>
+            <div className="col">Company CatchPhrase
+            <div className="col">{user?.company.catchPhrase}</div></div>
+            <div className="col">Company Bs
+            <div className="col">{user?.company.bs}</div></div>
           </div>
           <button type="submit" className="btn  btn-dark" onClick={()=>handleChange()}>TODOS</button>
         </div>
